a06abbb55f777723ff3aea57eacd98a72a26b22e
Vincenzo D'Amore <v.damore@gmail.com>
2015-01-04 02:55:35 +0100

    removed useless package, added missing apt-get update

--- recipes/common-packages.rb
+++ recipes/common-packages.rb
@@ -7,8 +7,38 @@
 # All rights reserved - Do Not Redistribute
 #
 
+
+case node['platform']
+when 'centos', 'redhat', 'fedora', 'amazon'
+  # yum check-update
+  execute "yum-check-update" do
+    command "yum check-update"
+    action :run
+  end
+when 'debian', 'ubuntu'
+  # apt-get-update
+  execute "apt-get-update" do
+    command "apt-get update"
+    action :run
+    # tip: to suppress this running every time, just use the apt cookbook
+    not_if do
+      ::File.exists?('/var/lib/apt/periodic/update-success-stamp') &&
+      ::File.mtime('/var/lib/apt/periodic/update-success-stamp') > Time.now - 86400*2
+    end
+  end
+end
+
+#
 package "curl"
+
+#
 package "git"
+
+#
 package "vim"
-package "nfs-kernel-server"
+
+#
+#package "nfs-kernel-server"
+
+#
 package "jsvc"
