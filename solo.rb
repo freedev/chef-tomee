0ffa140e283961ecdc3df4ab82498cef574bb5fd
Vincenzo D'Amore <v.damore@gmail.com>
2015-01-07 15:25:03 +0100

    fixed berks-cookbooks dependencies

--- solo.rb
+++ solo.rb
@@ -1,7 +1,7 @@
 path File.expand_path(File.dirname(__FILE__))
 file_cache_path "/root/chef-solo"
-# cookbook_path "#{File.expand_path(File.dirname(__FILE__))}/cookbooks"
-cookbook_path "/vagrant/berks-cookbooks"
+# cookbook_path "/vagrant/berks-cookbooks"
+cookbook_path "#{File.expand_path(File.dirname(__FILE__))}/berks-cookbooks"
 data_bag_path "#{File.expand_path(File.dirname(__FILE__))}/data_bags"
 log_level ENV['CHEF_LOG_LEVEL'] && ENV['CHEF_LOG_LEVEL'].downcase.to_sym || :info
 log_location ENV['CHEF_LOG_LOCATION'] || STDOUT
