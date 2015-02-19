eef2f8fb9fae0014c473fd6917b5823a31aaf5d8
Vincenzo D'Amore <v.damore@gmail.com>
2015-02-19 23:17:45 +0100

    fixed OpenSSLCookbook module

--- metadata.rb
+++ metadata.rb
@@ -4,11 +4,10 @@ maintainer_email "v.damore@gmail.com"
 license          'Apache 2.0'
 description      'Installs/Configures TomEE'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.8.0'
+version          '0.9.0'
 
-# depends 'apt', '~> 2.6.0'
-depends 'java', '~> 1.21'
-depends "openssl", ">= 0.0.0"
+depends 'java', '~> 1.31.0'
+depends 'openssl', '~> 3.0.2'
 # depends 'maven', '~> 1.2.0'
 # depends 'tomcat', '~> 0.17.0'
 # depends 'net-scp', '~> 1.2.1'
